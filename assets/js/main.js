8e998e28d194eff7b751b7d531e7efa0eebbcad4
maniswalking <kcsshy@gmail.com>
2025-04-01 16:29:57 +0900

    Save changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,37 @@ $(function () {
       prevEl: ".visual-prev",
     },
   });
+  
+  // 섹션02 - 슬라이드 swiper
+  /*
+  var swiper = new Swiper(".sec02-slide", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: ".sec02-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".sec02-next",
+      prevEl: ".sec02-prev",
+    },
+  });
+  */
+
+  var swiper = new Swiper(".sec02-slide", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    pagination: {
+      el: ".sec02-pagination",
+      type: "progressbar",
+    },
+  });
+
 
   // 섹션04 - 슬라이드 swiper
   var slideTotal = $('.portfolio_thumb .swiper-slide').length;
@@ -84,53 +115,82 @@ $(function () {
 
 
 // JavaScript - 메인비주얼 슬라이드 콘텐츠 자동 채우기
+
+
 let card_slide = [];
 
 $(window).on('load', function () {
   card_slide = [
     {
       marine_name: '흑기흉상어',
-      marine_explanation: '회색의 몸에 지느러미 무늬 끝부분이 검정색으로 되어 있어요. 인도•태평양: 홍해, 동아프리카, 하와이에 주로 서식하고 있어요~',
+      marine_txt01: '학명 : Carcharhinus melanopterus',
+      marine_txt02: '분류 : 척삭동물 > 연골어강 > 흉상어목 > 흉상어과',
+      marine_txt03: '빠밤~ 빠밤~ 빠밤 빠밤 빠밤~~~!!! 상어다~~',
+      marine_txt04: '지느러미 무늬 끝부분이 검정색으로 되어 있어요.',
       marine_image: './assets/images/marine-life-01-1x.png',
     },
     {
       marine_name: '흰동가리',
-      marine_explanation: '애니메이션(’니모를찾아서’)에 등장해 유명해진 흰동가리! 인도•태평양: 홍해, 동아프리카, 하와이에 주로 서식하고 있어요~',
+      marine_txt01: '학명 : Amphiprion ocellaris',
+      marine_txt02: '분류 : 척삭동물 > 경골어류 > 농어목 > 자리돔과',
+      marine_txt03: '애니메이션에 등장해 유명해진 흰동가리!',
+      marine_txt04: '말미잘과 공생하는 물고기랍니다.',
       marine_image: './assets/images/marine-life-02-1x.png',
     },
     {
       marine_name: '가든일',
-      marine_explanation: '겁이많은 정원 장어 가든일! 가든 일이 구멍 밖으로 몸을 내어놓고 있으면 마치 정원에 있는 갈대나 풀잎이 바람에 이리저리 흔들거리는 것처럼 보여서 이름이 "정원"이란 뜻의 "garden"에 "장어"란 뜻의 "eel"이 붙어 "정원장어"가 되었어요. 모랫바닥에 파고들어 꼬리를 모래 구멍에 넣은 상태에서 머리와 몸을 밖으로 길게 빼고 살아간답니다~',
+      marine_txt01: '학명 : Heteroconger hassi',
+      marine_txt02: '분류 : 척색동물문 > 조기아강 > 뱀장어목 > 붕장어과',
+      marine_txt03: '겁이많아 숨기 좋아하는 가든일!',
+      marine_txt04: '먹이를 먹을 때 조차 구멍을 절대 벗어나질 않아요.',
       marine_image: './assets/images/marine-life-03-1x.png',
     },
     {
       marine_name: '매너티',
-      marine_explanation: '하루 20kg의 먹이를 먹는 먹보 인어 매너티! 매너티는 주걱모양으로 둥글고 듀공은 고래처럼 두 갈래로 갈라져 있답니다',
+      marine_txt01: '학명 : Trichechus senegalensis',
+      marine_txt02: '분류 : 척삭동물 > 포유류 > 바다소목 > 매너티과',
+      marine_txt03: '하루 20kg의 먹이를 먹는 먹보 인어 매너티!',
+      marine_txt04: '매너티는 야채를 좋아한답니다.',
       marine_image: './assets/images/marine-life-04-1x.png',
     },
     {
       marine_name: '지브라상어',
-      marine_explanation: '어릴 때와 어른일 때의 모습이 다른 제브라 상어! 어릴 때는 몸의 무늬가 마치 얼룩말을 닮아 제브라 상어, 커서는 몸 무늬가 점박이로 바뀌어 레오파드 상어고 불려요',
+      marine_txt01: '학명 : Stegostoma fasciatum',
+      marine_txt02: '분류 : 척삭동물 > 연골어류 > 수염상어목 > 제브라상어과',
+      marine_txt03: '어릴 때와 어른일 때의 모습이 달라요.',
+      marine_txt04: '보기와 달리 온순한 성격을 가지고 있답니다.',
       marine_image: './assets/images/marine-life-05-1x.png',
     },
     {
       marine_name: '피라냐',
-      marine_explanation: '날카로운 이빨이 특징인 피라냐! 육식성으로 무리 지어 순식간에 먹이를 먹어치우는 것으로 유명해요',
+      marine_txt01: '학명 : Pygocentrus nattereri',
+      marine_txt02: '분류 : 척삭동물 > 경골어류 > 잉어목 > 카라신과',
+      marine_txt03: '날카로운 이빨이 특징인 피라냐!',
+      marine_txt04: '아마존 원주민들 말로 ‘이빨이 있는 물고기’라는 뜻이래요.',
       marine_image: './assets/images/marine-life-06-1x.png',
     },
     {
       marine_name: '아로와나',
-      marine_explanation: '아마존의 은빛 거구 아로와나! 크기가 1m 이상 자라나는 아로와나는 아마존이 고향이에요. 평소에는 수면 아래에서 느긋하게 헤엄치지만 먹이를 사냥하거나 흥분하면 수면 위로 점프하기도 해요.',
+      marine_txt01: '학명 : Osteoglossum bicirrhosum',
+      marine_txt02: '분류 : 척삭동물 > 조기어류 > 골설어목 > 골설어류과',
+      marine_txt03: '아마존의 은빛 거구 아로와나!',
+      marine_txt04: '아로와나는 크기가 1m 이상 자란답니다.',
       marine_image: './assets/images/marine-life-07-1x.png',
     },
     {
       marine_name: '훔볼트펭귄',
-      marine_explanation: '페루에서 온 신사 훔볼트펭귄! 펭귄은 추운 남극에서만 산다고 생각하기 쉽지만 훔볼트펭귄은 남아메리카 서해안에서 주로 분포해요',
+      marine_txt01: '학명 : Spheniscus humboldti',
+      marine_txt02: '분류 : 척삭동물 > 조류 > 펭귄목 > 펭귄과',
+      marine_txt03: '페루에서 온 신사 훔볼트펭귄!',
+      marine_txt04: '남극이 아니라 남아메리카 서해안에서 주로 분포해요',
       marine_image: './assets/images/marine-life-08-1x.png',
     },
     {
       marine_name: '캘리포니아 바다사자',
-      marine_explanation: '호기심이 많고 큰 눈을 가진 캘리포니아 바다사자! 바다사자의 특별한 수염은 한가닥 속에 10여개의 신경섬유를 가지고 있어서 매우 민감해요.',
+      marine_txt01: '학명 : Zalophus californianus',
+      marine_txt02: '분류 : 척삭동물 > 포유류 > 기각목 > 바다사자과',
+      marine_txt03: '호기심이 많고 큰 눈을 가진 캘리포니아 바다사자!',
+      marine_txt04: '수염을 이용하여 수중에서 먹잇감의 위치를 정확히 파악해요.',
       marine_image: './assets/images/marine-life-09-1x.png',
     },
   ];
@@ -141,13 +201,16 @@ $(window).on('load', function () {
 
     $(card).removeAttr('id');
     $(card).find('.marine-name').text(item.marine_name);
-    $(card).find('.marine-explanation').text(item.marine_explanation);
+    $(card).find('.marine-txt01').text(item.marine_txt01);
+    $(card).find('.marine-txt02').text(item.marine_txt02);
+    $(card).find('.marine-txt03').text(item.marine_txt03);
+    $(card).find('.marine-txt04').text(item.marine_txt04);
     $(card).find('.slide-bg img').attr('src', item.marine_image);
 
     $(target).append($(card));
   });
 
-  $('.card-slide > li:first-child').hide();
+  $('.card-slide > div:first-child').hide();
 });
 
     
