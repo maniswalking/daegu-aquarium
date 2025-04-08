19671345bb2154fc85fa335508b595e3609bc0d2
maniswalking <kcsshy@gmail.com>
2025-04-08 15:39:55 +0900

    Save for final

--- assets/js/script-sub2.js
+++ assets/js/script-sub2.js
@@ -7,22 +7,24 @@ $(function(){
     });
   //  스와이퍼
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1.7,
+        slidesPerView: 1,
         spaceBetween: 20,
         loop: true,
         // If we need pagination
         pagination: {
           el: ".swiper-pagination",
+          type: "progressbar",
         },
         breakpoints: {
-            1025: {  // 화면 크기 1024px 이상
-              slidesPerView: 1.7,
-              
-            },
             360: { // 화면 크기 360px 이상
               slidesPerView: 1,
               
             },
+            1024: {  // 화면 크기 1024px 이상
+              slidesPerView: 1.7,
+              
+            },
+
             
           }
     });
